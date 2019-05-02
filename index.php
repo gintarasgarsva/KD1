175c9d26cc37587325d26d5b3383f15ac9eecbf2
Gintaras <gintaras.garsva@gmail.com>
2019-05-02 15:27:21 +0300

    slot_run 15x15 dinaminis kubiku disco

--- index.php
+++ index.php
@@ -15,6 +15,46 @@ function slot_run($size) {
     return $array;
 }
 
-var_dump(slot_run(15));
+$array1 = slot_run(15);
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>KUBIKAI</title>
+        <meta http-equiv="refresh" content="1" >
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <style>
+            .blue{
+                border: 1px solid black;
+                background-color: blue;
+                width: 50px;
+                height: 50px;
+                display:inline-block;
+            }
+            .yellow{
+                border: 1px solid black;
+                background-color: yellow;
+                width: 50px;
+                height: 50px;
+                display:inline-block;
+            }
+        </style>
+    </head>
+    <body>
+        <?php foreach ($array1 as $index): ?>
+            <section>
+                <?php foreach ($index as $value): ?>
+                    <?php if ($value > 0): ?>
+                        <div class="blue"></div>
+                    <?php else: ?>
+                        <div class="yellow"></div>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            </section>
+        <?php endforeach; ?>
+    </body>
+</html> 
 
 
