a724929babd25e2c6f832638a38a9839cbb3fa83
Gintaras <gintaras.garsva@gmail.com>
2019-05-02 13:38:49 +0300

    Keiksmu_cenzura

--- index.php
+++ index.php
@@ -1,52 +1,23 @@
 <?php
 
-function slot_run() {
-    $array = [
-        [rand(0, 1), rand(0, 1), rand(0, 1)],
-        [rand(0, 1), rand(0, 1), rand(0, 1)],
-        [rand(0, 1), rand(0, 1), rand(0, 1)]
-    ];
-    return $array;
-}
+$text = "blt tas gaidys uzpiso mane.";
+$keiksmai = ['blt', 'uzpiso'];
+$cenzura = ['***', '******'];
 
-$array1 = slot_run();
+$new_text = str_replace($keiksmai, $cenzura, $text);
+
+print $new_text;
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
         <style>
-            .blue{
-                border: 1px solid black;
-                background-color: blue;
-                width: 50px;
-                height: 50px;
-                display:inline-block;
-
-            }
-            .yellow{
-                border: 1px solid black;
-                background-color: yellow;
-                width: 50px;
-                height: 50px;
-                display:inline-block;
-            }
+          
         </style>
     </head>
     <body>
-        <?php foreach ($array1 as $index): ?>
-            <section>
-                <?php foreach ($index as $value): ?>
-                    <?php if ($value > 0): ?>
-                        <div class="blue"></div>
-                    <?php else: ?>
-                        <div class="yellow"></div>
-                    <?php endif; ?>
-                <?php endforeach; ?>
-            </section>
-        <?php endforeach; ?>
-
-
+      
     </body>
 </html>
 
