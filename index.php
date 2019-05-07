0d7c41320abdabd2921d7d428b9df450dc5c2448
Gintaras <gintaras.garsva@gmail.com>
2019-05-07 11:03:50 +0300

    prekiu katalogas

--- index.php
+++ index.php
@@ -1,28 +1,85 @@
 <?php
 
-$panos = [
+$catalog = [
     [
-        'vardas' => 'Monika',
-        'grazi' => 'true',
-        'protinga' => 'false',
+        'name' => 'Batas',
+        'price' => 20,
+        'content' => 'Rudi batai iš naturalios kiaulės odos.',
+        'discount' => 20,
     ],
     [
-        'vardas' => 'Greta',
-        'grazi' => 'true',
-        'protinga' => 'true',
+        'name' => 'Striukė',
+        'price' => 75,
+        'content' => 'Odinė striukė iš veršelio odos.',
+        'discount' => 5,
     ],
     [
-        'vardas' => 'Egle',
-        'grazi' => 'false',
-        'protinga' => 'false',
+        'name' => 'Džinsai',
+        'price' => 45,
+        'content' => 'Plėšyti džinsai.',
+        'discount' => 25,
     ],
     [
-        'vardas' => 'Agne',
-        'grazi' => 'false',
-        'protinga' => 'true',
-    ]
+        'name' => 'Kepurė',
+        'price' => 15,
+        'content' => 'Pavasarinė kepurė',
+        'discount' => 5,
+    ],
+    [
+        'name' => 'Pirštinės',
+        'price' => 22,
+        'content' => 'Medžiaginės pirštinės',
+        'discount' => 35,
+    ],
 ];
 
-var_dump($panos);
+foreach ($catalog as $index => $value) {
+        $catalog[$index]['discount_price'] = $catalog[$index]['price'] * 
+            (100 - $catalog[$index]['discount']) / 100;
+}
 
 ?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hey Hey</title>
+    <style>
+        .produktas {
+            display: flex;
+            height: 50px;
+            text-align: center;
+            font-weight: bold;
+        }
+
+        .block {
+            border: 1px solid black;
+            flex: 1;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+</head>
+<body>
+<?php foreach ($catalog as $product): ?>
+    <div class="produktas">
+        <span class="block">
+            <?php print $product['name']; ?>
+        </span>
+        <span class="block">
+            <?php print $product['price'] . ' Eur.'; ?>
+        </span>
+        <span class="block">
+            <?php print $product['content']; ?>
+        </span>
+        <span class="block">
+            <?php print $product['discount'] . ' %'; ?>
+        </span>
+        <span class="block">
+            <?php print $product['discount_price'] . ' Eur.'; ?>
+        </span>
+    </div>
+<?php endforeach; ?>
+</body>
+</html>
+
