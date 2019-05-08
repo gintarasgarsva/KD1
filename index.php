fbcadca5393abf82b76ff7257850092d082f01b0
Gintaras <gintaras.garsva@gmail.com>
2019-05-08 16:16:59 +0300

    uzduotis #2

--- index.php
+++ index.php
@@ -1,34 +1,19 @@
 <?php
+$verte = 1;
 
-$array = ['b', 'x', 'x', 'b', 's'];
-
-function count_values($array, $val) {
-    $atrinktieji = 0;
-    foreach ($array as $value) {
-        if ($value == $val) {
-            $atrinktieji++;
-        }
-    }
-    return $atrinktieji;
+if (isset($_POST['bbd'])) {
+    $verte = $_POST['bbd'] + 1;
 }
 
-print count_values($array, 'x');
-
-
-function change_value(&$array, $val_from, $val_to) {
-    
-    $newArray = [];
-    foreach ($array as &$value) {
-        if ($value == $val_from) {
-            $value = $val_to;
-            $newArray[] = $value;
-        } else{
-            $newArray[] = $value;
-        }
-    }
-    
-}
-
-change_value($array, 'x', 'M');
-
-var_dump($array);
+?>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <form action="index.php" method="POST">
+            <button type="submit" name="bbd" value="<?php print $verte; ?>">
+                <?php print $verte; ?></button>
+        </form>
+    </body>
+</html>
