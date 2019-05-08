69b3604be0ccb7abc5eca231b1b6268b79b7c91e
Gintaras <gintaras.garsva@gmail.com>
2019-05-08 10:40:57 +0300

    Homework H1 ir H2. count_values ir change_value.

--- index.php
+++ index.php
@@ -1,11 +1,34 @@
 <?php
 
-$roll_joints = true;
+$array = ['b', 'x', 'x', 'b', 's'];
 
-$joint1 = &$roll_joints;
-$joint2 = &$joint1;
-$joint3 = &$joint2;
+function count_values($array, $val) {
+    $atrinktieji = 0;
+    foreach ($array as $value) {
+        if ($value == $val) {
+            $atrinktieji++;
+        }
+    }
+    return $atrinktieji;
+}
 
-print $joint1;
-print $joint2;
-print $joint3;
+print count_values($array, 'x');
+
+
+function change_value(&$array, $val_from, $val_to) {
+    
+    $newArray = [];
+    foreach ($array as &$value) {
+        if ($value == $val_from) {
+            $value = $val_to;
+            $newArray[] = $value;
+        } else{
+            $newArray[] = $value;
+        }
+    }
+    
+}
+
+change_value($array, 'x', 'M');
+
+var_dump($array);
